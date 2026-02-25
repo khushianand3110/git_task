bf27d9e46bc7a3c4ef6b552515055faa06324b1c
khushianand3110 <khushi.anand@cbnits.com>
2026-02-25 13:17:12 +0530

    WIP: Refactor core utility.

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 print("1 = Red")
 print("2 = Yellow")
 print("3 = Green")
+print("4 = Pedestrian Crossing")
+print("5 = Emergency Vehicle")
 
 choice = int(input("Enter number: "))
 
 if choice == 1:
-    print("STOP")
+    print("STOP - Vehicles must stop")
 elif choice == 2:
-    print("WAIT")
+    print("WAIT - Get ready")
 elif choice == 3:
-    print("GO")
+    print("GO - Vehicles can move")
+elif choice == 4:
+    print("Pedestrian Crossing - Let people walk")
+elif choice == 5:
+    print("Emergency Mode - Give way immediately")
 else:
     print("Wrong choice")
 
