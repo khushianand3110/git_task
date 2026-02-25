a6107d863426bafc6f92e41677e5cd16cf0ef78d
khushianand3110 <khushi.anand@cbnits.com>
2026-02-25 12:31:39 +0530

    create traffic system

---
+++ main.py
@@ -0,0 +1,14 @@
+print("1 = Red")
+print("2 = Yellow")
+print("3 = Green")
+
+choice = int(input("Enter number: "))
+
+if choice == 1:
+    print("STOP")
+elif choice == 2:
+    print("WAIT")
+elif choice == 3:
+    print("GO")
+else:
+    print("Wrong choice")
